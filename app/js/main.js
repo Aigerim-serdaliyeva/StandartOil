52992801afeb1da722c7ea9526f90d82e4967f3d
Сарсенбаев Нурбол <kuma_official@mail.ru>
2018-07-20 15:39:22 +0600

    s-shop

--- app/js/main.js
+++ app/js/main.js
@@ -198,6 +198,34 @@ $(document).ready(function () {
     },
   });
 
+  $(".carousel-shop").slick({
+    centerMode: true,
+    centerPadding: '0px',
+    slidesToShow: 5,
+    dots: true,
+    arrows: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  })
+
 
 });
 
